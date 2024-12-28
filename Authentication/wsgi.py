98c64606caaadec67c9431248e8423e2af0ff8bb
vamsikrishna2706 <20bq1a0215@vvit.net>
2024-12-28 19:38:12 +0530

    final commit

--- Authentication/wsgi.py
+++ Authentication/wsgi.py
@@ -1,7 +1,7 @@
 """
 WSGI config for Authentication project.
 
-It exposes the WSGI callable as a module-level variable named ``application``.
+It exposes the WSGI callable as a module-level variable named `application`.
 
 For more information on this file, see
 https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
